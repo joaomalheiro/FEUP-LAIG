932c2ef137929ec4886a92c35dad10415649fd34
Unknown <acruz9823@gmail.com>
2018-10-19 00:38:17 +0100

    Fixed Error on Triangle primitive

--- MyTriangle.js
+++ MyTriangle.js
@@ -50,26 +50,26 @@ class MyTriangle extends CGFobject {
 	var p2 = this.p2;
 	var p3 = this.p3;
 
-	this.a = Math.sqrt(math.pow(p1[0] - p3[0],2) +
-			 		  math.pow(p1[1] - p3[1],2) +
-			 		  math.pow(p1[2] - p3[2],2));
+	this.a = Math.sqrt(Math.pow(p1[0] - p3[0],2) +
+			 		  Math.pow(p1[1] - p3[1],2) +
+			 		  Math.pow(p1[2] - p3[2],2));
 
-	this.b = Math.sqrt(math.pow(p2[0] - p1[0],2) +
-			 		   math.pow(p2[1] - p1[1],2) +
-			 		   math.pow(p2[2] - p1[2],2));
+	this.b = Math.sqrt(Math.pow(p2[0] - p1[0],2) +
+			 		   Math.pow(p2[1] - p1[1],2) +
+			 		   Math.pow(p2[2] - p1[2],2));
 
-	this.c = Math.sqrt(math.pow(p3[0] - p2[0],2) +
-			 		   math.pow(p3[1] - p2[1],2) +
-			 		   math.pow(p3[2] - p2[2],2));
+	this.c = Math.sqrt(Math.pow(p3[0] - p2[0],2) +
+			 		   Math.pow(p3[1] - p2[1],2) +
+			 		   Math.pow(p3[2] - p2[2],2));
 
-	this.cosBeta =  ( math.pow(this.a,2) - math.pow(this.b,2)+ math.pow(this.c,2)) / (2 * this.a * this.c);
+	this.cosBeta =  ( Math.pow(this.a,2) - Math.pow(this.b,2)+ Math.pow(this.c,2)) / (2 * this.a * this.c);
 
 	this.beta = Math.acos(this.cosBeta);
 
 	this.texCoords = [
 	
-		(this.c - this.a*Math.cos(beta)) / lthis.length_s, 
-		1-(this.a * Math.sin(beta))/this.length_t,
+		(this.c - this.a*Math.cos(this.beta)) / this.length_s, 
+		1-(this.a * Math.sin(this.beta))/this.length_t,
 		0, 1/this.length_t, this.c/this.length_s,
 		1/this.length_t
 		
