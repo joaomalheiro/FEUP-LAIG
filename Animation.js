b939d184c346f78a624153421efdc4064e6a5936
up201603526@FEUPSIG.FE.UP.PT <up201603526@FEUPSIG.FE.UP.PT>
2018-11-07 11:24:07 +0000

    Added Linear Animation class

--- Animation.js
+++ Animation.js
@@ -1,13 +1,36 @@
 class Animation {
 
-    constructor(scene, id, speed) {
+    constructor(scene, id, time) {
         this.scene = scene;
         this.id = id;
-        this.speed = speed;
+        this.time = time;
         this.done = false;
+        this.currentTime = 0;
     }
 
     isDone() {
         return this.done;
+    }
+}
+
+class LinearAnimation extends Animation {
+    constructor(scene,id,time,controlPoints){
+        this.controlPoints = controlPoints;
+        super(scene,id,time);
+    }
+
+    update(deltaTime) {
+        super.currentTime += deltaTime;
+        let animPercentage = super.currentTime / super.time;
+        let animState = this.controlPoints.length * animPercentage;
+        let controlPoint = Math.floor(animState);
+        let percentageControlPoint = animState - controlPoint;
+
+        let vecDistance = vec3.subtract(this.controlPoints[controlPoint],this.controlPoints[controlPoint + 1]);
+        vecDistance *= percentageControlPoint;
+
+        let mat = mat4.create();
+        mat4.translate(mat,mat,vecDistance);
+        scene.multMatrix(matrix);
     }
 }
