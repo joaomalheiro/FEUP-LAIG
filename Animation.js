3a7085a4c2b993f14236560befcc958b0c9daf65
Unknown <acruz9823@gmail.com>
2018-11-09 20:11:22 +0000

    Linear Animation now working with multiple control points

--- Animation.js
+++ Animation.js
@@ -21,28 +21,27 @@ class Animation {
 class LinearAnimation extends Animation {
     constructor(scene,id,time,controlPoints){
         super(scene,id,time);
-        //console.log(this.time);
         this.controlPoints = controlPoints;
         this.totalDist = 0;
-        //The distance between control points
+        //The increasing distance between control points
         this.vecDists = [];
         for(let i = 0; i < controlPoints.length - 1 ; i++) {
-            this.vecDists[i] = Math.abs(vec3.distance(controlPoints[i],controlPoints[i + 1]));
-            this.totalDist += this.vecDists[i];
-           // console.log(this.vecDists[i],i);
+            let vectorDistance = Math.abs(vec3.distance(controlPoints[i],controlPoints[i + 1]));
+            this.totalDist += vectorDistance;
+            this.vecDists[i] = this.totalDist;
         }
     }
 
     update(deltaTime) {
+        //Updates the animation counter, if it reaches the end it resets
         this.currentTime += deltaTime;
         if(this.currentTime > this.time) {
             this.currentTime = 0;
         }
+        //How much distance the animation has travelled
         let currentDist = (this.currentTime / this.time) * this.totalDist;
-        console.log(currentDist / this.totalDist * 100);
         //Determines the controlPoint where the animation is ahead of
         let controlPoint;
-        //console.log(this.vecDists[0]);
         for(let i = 0; i < this.vecDists.length; i++){
             if(currentDist < this.vecDists[i]){
                 controlPoint = i;
@@ -56,16 +55,19 @@ class LinearAnimation extends Animation {
             distBetweenPoints = currentDist - this.vecDists[controlPoint - 1];
         }
 
+        //Translating the scene to the current animation point
         let mat = mat4.create();
-        //console.log(this.controlPoints[controlPoint]);
         mat4.translate(mat,mat,this.controlPoints[controlPoint]);
-
+        //The vector made by the 2 control points where the object is between
         let activeVector = vec3.create();
         activeVector = vec3.subtract(activeVector,this.controlPoints[controlPoint + 1],this.controlPoints[controlPoint]);
-        console.log(distBetweenPoints , this.vecDists[controlPoint]);
-        let distInVec = distBetweenPoints / this.vecDists[controlPoint];
+
+        let activeVectorSize = this.vecDists[controlPoint];
+        if(controlPoint != 0) {
+            activeVectorSize = this.vecDists[controlPoint] - this.vecDists[controlPoint - 1];
+        }
+        let distInVec = distBetweenPoints / activeVectorSize;
         vec3.multiply(activeVector,activeVector,vec3.fromValues(distInVec,distInVec,distInVec));
-        console.log(activeVector);
         mat4.translate(mat,mat,activeVector);
         this.matrix = mat;
     }
