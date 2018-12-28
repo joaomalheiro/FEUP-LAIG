1859af730a55f37d61e261c749fe6b9e96f2b05c
Unknown <acruz9823@gmail.com>
2018-12-28 17:11:53 +0000

    Improved dead bishops position

--- project3/primitives/Board.js
+++ project3/primitives/Board.js
@@ -159,7 +159,7 @@ class Board extends CGFobject {
             if(active){
                 this.scene.translate(-4.5 + x ,0,-4.5 + z);   
             } else {
-                this.scene.translate(-6.5,0,-4.5 + this.whiteBishops[i].deadId);
+                this.scene.translate(-5.5 - Math.floor(this.whiteBishops[i].deadId / 10),0,-4.5 + (this.whiteBishops[i].deadId % 10));
             }
             this.scene.scale(0.55,0.70,0.55);
             this.whiteBishops[i].display();
@@ -177,7 +177,7 @@ class Board extends CGFobject {
             if(active){
                 this.scene.translate(-4.5 + x ,0,-4.5 + z);   
             } else {
-               this.scene.translate(6,0,-4.5 + this.blackBishops[i].deadId);
+               this.scene.translate(5.5 + Math.floor(this.blackBishops[i].deadId / 10),0,-4.5 + (this.blackBishops[i].deadId % 10));
             }
             this.scene.scale(0.55,0.70,0.55);
             this.blackBishops[i].display();
