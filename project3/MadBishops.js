6ed5084cc952e1bbda997d8f2e6b686613797091
João Malheiro <jonas_malheiro_sousa@hotmail.com>
2018-12-30 23:13:51 +0000

    fixed

--- project3/MadBishops.js
+++ project3/MadBishops.js
@@ -39,7 +39,7 @@ class MadBishops extends CGFobject {
             this.board.makeMove(startRow,startColumn,endRow,endColumn);
             this.playerTurn = (this.playerTurn % 2) + 1;
             console.log(this.playerTurn);
-            serverMove(startRow,startColumn,endRow,endColumn,this.boardState,this.whitePieces,this.blackPieces,this.playerTurn, data2 => this.serverMoveHandler(data2));
+            serverMove(startColumn,startRow,endColumn,endRow,this.boardState,this.whitePieces,this.blackPieces,this.playerTurn, data2 => this.serverMoveHandler(data2));
         } else 
             console.log('Invalid Move',startRow,startColumn,endRow,endColumn);
     }
